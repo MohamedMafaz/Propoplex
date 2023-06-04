ac68667a006f06a6c855335e7420ca5cbb03a90c
Mohamed Mafaz P R <113255500+MohamedMafaz@users.noreply.github.com>
2023-06-04 12:52:54 +0530

    Fixed error form not showing

--- main.js
+++ main.js
@@ -1,52 +1,52 @@
-// Code for BardAPI
-import Bard, { askAI } from "bard-ai";
+// // Code for BardAPI
+// import Bard, { askAI } from "bard-ai";
 
-var platform = document.getElementById("platform").value;
-var name = document.getElementById("name").value;
-var description = document.getElementById("desc").value;
-var role = document.getElementById("role").value;
-var price = document.getElementById("price").value;
-var experience = document.getElementById("experience").value;
-var pastwork = document.getElementById("pastwork").value;
-var link = document.getElementById("link").value;
-var goodfit = document.getElementById("goodfit").value;
-var words = document.getElementById("words").value;
+// var platform = document.getElementById("platform").value;
+// var name = document.getElementById("name").value;
+// var description = document.getElementById("desc").value;
+// var role = document.getElementById("role").value;
+// var price = document.getElementById("price").value;
+// var experience = document.getElementById("experience").value;
+// var pastwork = document.getElementById("pastwork").value;
+// var link = document.getElementById("link").value;
+// var goodfit = document.getElementById("goodfit").value;
+// var words = document.getElementById("words").value;
 
-var prompt =
-  "${description} I am going to apply for this job. write me an attractive proposal for ${platform} in about ${words} with given details. 1) ${name} 2) Role ${role} 3)  price ${price} 4) I have ${experience} Years of experience in this field. 5) Attached Past works ${pastwork} 6) link for the portfolio ${portfolio}. I am good fit for this job because ${goodfit}. Write me an attactive proposal with this details. Write it about 150 words. End with strong CTA";
+// var prompt =
+//   "${description} I am going to apply for this job. write me an attractive proposal for ${platform} in about ${words} with given details. 1) ${name} 2) Role ${role} 3)  price ${price} 4) I have ${experience} Years of experience in this field. 5) Attached Past works ${pastwork} 6) link for the portfolio ${portfolio}. I am good fit for this job because ${goodfit}. Write me an attactive proposal with this details. Write it about 150 words. End with strong CTA";
 
-await Bard.init(
-  "XQiIjGefHBGXO_fJH7_H9WpGimggRbpseEJOaQQugmoDjZLaUS2Tr1Dw_HKnrarPFCFInQ."
-);
+// await Bard.init(
+//   "XQiIjGefHBGXO_fJH7_H9WpGimggRbpseEJOaQQugmoDjZLaUS2Tr1Dw_HKnrarPFCFInQ."
+// );
 
-console.log(await askAI(prompt));
+// console.log(await askAI(prompt));
 
 // Summa
-function selectCard(button) {
-  let card = button.parentNode;
-  // Remove "selected" class from all cards except the clicked one and change button text to "USE NOW"
-  let cards = document.querySelectorAll(".card");
-  cards.forEach(function (c) {
-    if (c !== card && c.classList.contains("selected")) {
-      c.classList.remove("selected");
-      //gets the last children of the card (the button) and change the text
-      c.children.item(c.children.length - 1).innerHTML = "USE NOW";
-    }
-  });
+// function selectCard(button) {
+//   let card = button.parentNode;
+//   // Remove "selected" class from all cards except the clicked one and change button text to "USE NOW"
+//   let cards = document.querySelectorAll(".card");
+//   cards.forEach(function (c) {
+//     if (c !== card && c.classList.contains("selected")) {
+//       c.classList.remove("selected");
+//       //gets the last children of the card (the button) and change the text
+//       c.children.item(c.children.length - 1).innerHTML = "USE NOW";
+//     }
+//   });
 
-  // Add "selected" class to clicked card
-  card.classList.add("selected");
+//   // Add "selected" class to clicked card
+//   card.classList.add("selected");
 
-  // change button text to Selected
-  button.innerHTML = "Selected";
-}
-let navbar = document.getElementById("navbarId");
-function myFunction() {
-  navbar.classList.add("navbar1");
-}
-function closeFunction() {
-  navbar.classList.remove("navbar1");
-}
+//   // change button text to Selected
+//   button.innerHTML = "Selected";
+// }
+// let navbar = document.getElementById("navbarId");
+// function myFunction() {
+//   navbar.classList.add("navbar1");
+// }
+// function closeFunction() {
+//   navbar.classList.remove("navbar1");
+// }
 
 // Code for the form needs to be same as selected card
 function selectCard(button) {
