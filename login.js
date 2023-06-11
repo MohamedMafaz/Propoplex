b518f5b7b6908bc408714b4e81403d2cae3f6123
Mohamed Mafaz P R <113255500+MohamedMafaz@users.noreply.github.com>
2023-06-11 10:19:34 +0530

    Test

--- login.js
+++ login.js
@@ -1,7 +1,12 @@
-let navbar = document.getElementById("navbarId");
-function myFunction() {
-    navbar.classList.add("navbar1");
-}
-function closeFunction() {
-    navbar.classList.remove("navbar1");
-}
+function rotateTestimonials() {
+    const cards = document.getElementsByClassName('testimonial-card');
+    const activeCard = document.querySelector('.testimonial-card.active');
+    const activeIndex = Array.from(cards).indexOf(activeCard);
+    const nextIndex = (activeIndex + 1) % cards.length;
+  
+    activeCard.classList.remove('active');
+    cards[nextIndex].classList.add('active');
+  }
+  
+  setInterval(rotateTestimonials, 5000);
+  
